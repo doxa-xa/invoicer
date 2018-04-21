39380c0101ac24269bbedfc2da5b4bc45752e953
unknown <c.todopob@gmail.com>
2018-04-21 11:39:32 +0300

    index page language UI

---
+++ assets/lang.js
@@ -0,0 +1,108 @@
+const lang = {
+  es:{
+    h1:'Emita una factura gratis',
+    form:{
+      label:'Idioma',
+      select:['English','Български','Français','Español','Deutsch','Ελληνικά','Polskie','Italiano'],
+      submit:'Elegir'
+    },
+    login:'Inicia con',
+    buttons:{
+      sUp:'Regístrate',
+      sIn:'Registrarse'
+    }
+  },
+  bg:{
+    h1:'Издайте фактура безплатно',
+    form:{
+      label:'Език',
+      select:['English','Български','Français','Español','Deutsch','Ελληνικά','Polskie','Italiano'],
+      submit:'Избери'
+    },
+    login:'Влез със',
+    buttons:{
+      sUp:'Регистрация',
+      sIn:'Вход'
+    }
+  },
+  en:{
+    h1:'Issue an invoice for free',
+    form:{
+      label:'Language',
+      select:['English','Български','Français','Español','Deutsch','Ελληνικά','Polskie','Italiano'],
+      submit:'Choose'
+    },
+    login:'Log in with',
+    buttons:{
+      sUp:'Sign up',
+      sIn:'Sign in'
+    }
+  },
+  fr:{
+    h1:'Émettre une facture gratuitement',
+    form:{
+      label:'La langue',
+      select:['English','Български','Français','Español','Deutsch','Ελληνικά','Polskie','Italiano'],
+      submit:'Choisir'
+    },
+    login:'Connectez-vous avec',
+    buttons:{
+      sUp:"S'inscrire",
+      sIn:'Se connecter'
+    }
+  },
+  gr:{
+    h1:'Εκδώστε ένα τιμολόγιο δωρεάν',
+    form:{
+      label:'Γλώσσα',
+      select:['English','Български','Français','Español','Deutsch','Ελληνικά','Polskie','Italiano'],
+      submit:'Επιλέγω'
+    },
+    login:'Συνδέσου με',
+    buttons:{
+      sUp:'Εγγραφείτε',
+      sIn:'Συνδεθείτε'
+    }
+  },
+  it:{
+    h1:'Emettere una fattura gratuitamente',
+    form:{
+      label:'Linguaggio',
+      select:['English','Български','Français','Español','Deutsch','Ελληνικά','Polskie','Italiano'],
+      submit:'Scegliere'
+    },
+    login:'Entra con',
+    buttons:{
+      sUp:'Iscriviti',
+      sIn:'Registrati'
+    }
+  },
+  pl:{
+    h1:'Wystaw fakturę za darmo',
+    form:{
+      label:'Język',
+      select:['English','Български','Français','Español','Deutsch','Ελληνικά','Polskie','Italiano'],
+      submit:'Wybierać'
+    },
+    login:'Zaloguj Się',
+    buttons:{
+      sUp:'Zapisz się',
+      sIn:'Zaloguj'
+    }
+  },
+  de:{
+    h1:'Erstellen Sie eine Rechnung kostenlos',
+    form:{
+      label:'Sprache',
+      select:['English','Български','Français','Español','Deutsch','Ελληνικά','Polskie','Italiano'],
+      submit:'Wählen'
+    },
+    login:'Einloggen mit',
+    buttons:{
+      sUp:'Anmelden',
+      sIn:'Eintragen'
+    }
+  },
+};
+
+module.exports = lang;
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,54 @@
 const appRoutes = require('express').Router();
+const bodyParser = require('body-parser');
+const language = require('../assets/lang.js');
+
+var urlencodedparser = bodyParser.urlencoded({extended:false});
 
 //routes [START]
 appRoutes.get('/',(req,res)=>{
-  res.status(200).render('index');
+  res.status(200).render('index',{lang:language.en});
 });
+//language choosing option POST request
+appRoutes.post('/',urlencodedparser,(req,res)=>{
+  switch (req.body.selectLang){
+    case 'fr': res.redirect('/fr'); break;
+    case 'es': res.redirect('/es'); break;
+    case 'de': res.redirect('/de'); break;
+    case 'bg': res.redirect('/bg'); break;
+    case 'gr': res.redirect('/gr'); break;
+    case 'pl': res.redirect('/pl'); break;
+    case 'it': res.redirect('/it'); break;
+    case 'en': res.redirect('/en'); break;
+    default : res.redirect('/en'); break;
+  }
+});
+//language UI redirects [START]
+appRoutes.get('/fr',(req,res)=>{
+  res.status(200).render('index',{lang:language.fr});
+});
+appRoutes.get('/es',(req,res)=>{
+  res.status(200).render('index',{lang:language.es});
+});
+appRoutes.get('/de',(req,res)=>{
+  res.status(200).render('index',{lang:language.de});
+});
+appRoutes.get('/bg',(req,res)=>{
+  res.status(200).render('index',{lang:language.bg});
+});
+appRoutes.get('/pl',(req,res)=>{
+  res.status(200).render('index',{lang:language.pl});
+});
+appRoutes.get('/it',(req,res)=>{
+  res.status(200).render('index',{lang:language.it});
+});
+appRoutes.get('/en',(req,res)=>{
+  res.status(200).render('index',{lang:language.en});
+});
+appRoutes.get('/gr',(req,res)=>{
+  res.status(200).render('index',{lang:language.gr});
+});
+//language UI redirects [END]
+
 //routes [END]
 
 
